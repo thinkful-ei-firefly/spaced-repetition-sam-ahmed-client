5a30c369c514a4fd6f17e718d4dd53a09716ed1b
Sam Walker <jack.nasal@gmail.com>
2019-10-16 16:18:05 -0400

    made everything work with the tests

--- src/components/AnswerSubmit/AnswerSubmit.js
+++ src/components/AnswerSubmit/AnswerSubmit.js
@@ -14,7 +14,7 @@ class AnswerSubmit extends React.Component {
     this.context.setGuess(guess)
     ApiLanguageService.submitAnswer(guess, nextWord, id)
       .then(res => {
-        this.context.setHead({totalScore: res.total_score,nextWord:res.nextWord, wordCorrectCount:res.correct_count, wordIncorrectCount:res.incorrect_count  })
+        this.context.setHead({totalScore: res.total_score})
         this.context.setAnswerResponse(res)
         this.context.setCorrect({correct: res.correct})
       })
