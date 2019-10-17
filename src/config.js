43fe85f317be4630b37b3ac3e52a3afd72731371
Ahmed <ahmed.max7586@gmail.com>
2019-10-17 16:20:30 -0400

    deployed

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  // API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://spaced-repetition-sam-ahmed.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
